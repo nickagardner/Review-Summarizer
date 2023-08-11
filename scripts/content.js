f0254327e0bb573dadc612b797a41203a394c3d2
ng <nickgardner7777@gmail.com>
2023-08-10 22:22:21 -0400

    amazon blocking scraping. will try to pivot to amazon product advertising api

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,13 @@
-console.log($("html").html());
+let url = $(location).attr("href");
+
+const regex = "dp\/.*\/";
+const product_id = url.match(regex)[0].split("\/")[1];
+
+$.get( "http://amazon.com/product-reviews/".concat(product_id), function( data ) {
+    console.log(data);     
+});
+
+
+// $('div[data-hook=review-collapsed]').each(function(i, obj) {
+//   console.log($( this ).text());
+// });
