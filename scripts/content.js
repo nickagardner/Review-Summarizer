117014fab343d0fe46cef3b6f289c91979a1b712
ng <nickgardner7777@gmail.com>
2023-08-12 22:33:26 -0400

    scraping functionality successful through JS. update project plan

--- scripts/content.js
+++ scripts/content.js
@@ -3,11 +3,14 @@ let url = $(location).attr("href");
 const regex = "dp\/.*\/";
 const product_id = url.match(regex)[0].split("\/")[1];
 
-$.get( "http://amazon.com/product-reviews/".concat(product_id), function( data ) {
-    console.log(data);     
-});
+let reviews = [];
 
+for (i=1; i<11; i++) {
+    $.get("https://amazon.com/product-reviews/".concat(product_id) + "/ref=cm_cr_arp_d_paging_btm_next_" + i.toString() + "?ie=UTF8&reviewerType=all_reviews&pageNumber=" + i.toString(), function( data ) {
+        $(data).find('span[data-hook=review-body]').each(function(i, obj) {
+            reviews.push($(this).text().trim());
+        });
+    });
+};
 
-// $('div[data-hook=review-collapsed]').each(function(i, obj) {
-//   console.log($( this ).text());
-// });
+console.log(reviews);
