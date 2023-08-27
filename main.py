9983cec93e21de3f88f176135e083efb19a56f7e
ng <nickgardner7777@gmail.com>
2023-08-27 17:04:41 -0400

    clean up prompts

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def _summarize(req_info):
             return text.strip().split(", ")
     
     template = """You are a helpful assistant who summarizes product reviews. 
-    A user will pass in a list of reviews, and you should generate 5 common positive themes and 5 common negative themes present in the reviews as a comma separated list.
+    A user will pass in a list of reviews, and you should generate 5 common positive themes as a comma separated list.
     ONLY return a comma separated list, and nothing more."""
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
     human_template = "{text}"
@@ -43,15 +43,35 @@ def _summarize(req_info):
         output_parser=CommaSeparatedListOutputParser()
     )
     
-    themes = chain.run(req_info[1:])
+    pos_themes = chain.run(req_info[1:])
+
+    template = """You are a helpful assistant who summarizes product reviews. 
+    A user will pass in a list of reviews, and you should generate 5 common negative themes as a comma separated list.
+    ONLY return a comma separated list, and nothing more."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    chain = LLMChain(
+        llm=ChatOpenAI(openai_api_key=req_info[0]),
+        prompt=chat_prompt,
+        output_parser=CommaSeparatedListOutputParser()
+    )
+    
+    neg_themes = chain.run(req_info[1:])
+
+    themes = pos_themes.extend(neg_themes)
 
     template = """You are a helpful assistant who counts how many times themes appear in a list of reviews. 
     A user will pass in a list of reviews, and a list of 10 themes. For each review, you will determine whether each theme is present. 
     You will return a list of counts where each count corresponds with the number of times each theme appeared in the reviews. 
     ONLY return a comma separated list, and nothing more."""
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-    human_template = "{text}"
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    human_template = """Reviews: {reviews}
+    Themes: """
+    human_message_prompt = HumanMessagePromptTemplate.from_template(input_variables=["reviews", "themes"],
+                                                                    template=human_template)
     
     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
     chain = LLMChain(
@@ -60,6 +80,9 @@ def _summarize(req_info):
         output_parser=CommaSeparatedListOutputParser()
     )
 
-    counts = chain.run(req_info[1:])
+    counts = chain.run({
+    'reviews': reviews,
+    'themes': themes
+    }))
 
     return (themes, counts)
