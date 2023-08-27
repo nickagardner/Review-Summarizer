0b11046883b44eb47181bcbc343f2a659590151a
ng <nickgardner7777@gmail.com>
2023-08-27 15:30:03 -0400

    add basic summarization func

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 from fastapi import FastAPI, Request
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from langchain.schema import BaseOutputParser
+
 app = FastAPI()
 
 @app.post("/summarize")
 async def summarize(info : Request):
     req_info = await info.json()
+    output = _summarize(req_info)
     return {
         "status" : "SUCCESS",
-        "data" : req_info,
+        "data" : output,
     }
+
+
+def _summarize(req_info)
+    class CommaSeparatedListOutputParser(BaseOutputParser):
+        """Parse the output of an LLM call to a comma-separated list."""
+    
+    
+        def parse(self, text: str):
+            """Parse the output of an LLM call."""
+            return text.strip().split(", ")
+    
+    template = """You are a helpful assistant who summarizes product reviews. 
+    A user will pass in a list of reviews, and you should generate 5 common themes present in the reviews as a comma separated list.
+    ONLY return a comma separated list, and nothing more."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    chain = LLMChain(
+        llm=ChatOpenAI(),
+        prompt=chat_prompt,
+        output_parser=CommaSeparatedListOutputParser()
+    )
+    return chain.run(req_info)
